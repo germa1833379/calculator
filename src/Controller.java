4002fd3804f8b99562f530fc55472e241bcb4211
Mattys Gervais <mattysgervais@gmail.com>
2019-11-22 10:48:05 -0500

    Texte écrit, debut du sorting...

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,4 @@
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
@@ -6,9 +7,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.net.URL;
-import java.util.ResourceBundle;
-
+import java.util.*;
 
 public class Controller implements Initializable
 {
@@ -19,7 +21,9 @@ public class Controller implements Initializable
     String curr="";
     @FXML
     public void add(ActionEvent event){
-        curr+=((Button)event.getSource()).getText();
+        String added = ((Button)event.getSource()).getText();
+
+        curr+=added;
         calc.setValue(curr);
     }
     @Override
@@ -27,7 +31,49 @@ public class Controller implements Initializable
         label.textProperty().bind(calc);
     }
     public void calculate(){
+        char[] input= curr.toCharArray();
+        ArrayList<Character> finalArray = new ArrayList<>();
+        ArrayList<Character> temp = new ArrayList<>();
+        String lol = "";
+        for(int i=0;i<input.length;i++){
+            char curr=input[i];
+            if(Character.isDigit(curr)||curr=='e'||curr=='π'){
+                finalArray.add(curr);
+            }else if (curr=='*'){
 
+            }else if (curr=='/'){
+
+            }else if (curr=='^'){
+
+            }else if (curr=='-'){
+
+            }else if (curr=='+'){
+                temp.add(curr);
+            }else if (curr=='s'||curr=='i'||curr=='n'||curr=='o'||curr=='t'||curr=='a'||curr=='r'){
+                lol+=(Character.toString(curr));
+               if(lol.equals("sin")) {
+                   temp.add('s');
+                   lol="";
+               }
+                if(lol.equals("cos")) {
+                    temp.add('c');lol="";
+                }
+                if(lol.equals("tan")) {
+                    temp.add('t');lol="";
+                }if(lol.equals("arcsin")) {
+                    temp.add('j');lol="";
+                }if(lol.equals("arccos")) {
+                    temp.add('k');lol="";
+                }
+                if(lol.equals("arctan")) {
+                    temp.add('l');lol="";
+                }
+                if(lol.equals("rac")) {
+                    temp.add('r');lol="";
+                }
+
+            }
+        }
     }
     public void remove(){
 
